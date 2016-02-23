e83f1b4ca5d49f0a065aff6ff004a99f45761c64
uday4a9 <uday4a9@gmail.com>
2016-02-23 08:18:44 +0530

    After adding the stack.c

---
+++ stack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+
+#define HALFMAX 10
+#define MAX 20
+
+// stack follows only LIFO order
+// queue follows only FIFO order, Need to implement it 
+// using LIFO
+
+int stack[MAX]; 
+int queue1[HALFMAX], queue2[HALFMAX];
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
